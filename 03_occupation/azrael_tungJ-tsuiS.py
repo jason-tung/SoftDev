6af3540015fc857e0f0ba0e34887a1334b02fa89
Jason Tung <mickeu420@gmail.com>
2018-09-13 14:49:27 -0400

    added 03_occupation

---
+++ 03_occupation/azrael_tungJ-tsuiS.py
@@ -0,0 +1,29 @@
+# azrael - Jason Tung and Simon Tsui
+# SoftDev1 pd8
+# K06 -- StI/O: Divine your Destiny!
+# 2018-09-13
+
+# In the notes&code repo is a .csv file containing information about occupations in the United States (courtesy of Mr. Brooks). Here is a snippet of two lines of the file:
+#
+#
+#     Legal,0.8
+#
+#     "Education, training and library",6.1
+#
+#
+# The first item is the name of the occupation and the second is the percentage of the U.S. workforce it comprises. Note the quotation marks surrounding an occupation containing commas.
+#
+#
+# Your job (as a DUO):
+#
+# Write a Python script to read in the file and build an appropriate dictionary from it. Make sure the percentages are stored as numbers.
+#
+# Create a function that returns a randomly selected occupation where the results are weighted by the percentage given. For example, there should be a 6.1% chance that "Education, training and library" is returned.
+#
+#
+# Store this in a directory named 03_occupation in your workshop repo.
+# Design first. Conceive a plan with your partner. KtS are your friends.
+# Develop jointly. Once finished, store identical code in each of your respective workshops.
+# Team name must portend greatness.
+
+#
