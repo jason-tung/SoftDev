572c2dff9abeb891c14e2cceb10c76a739afd6a3
Jason Tung <mickeu420@gmail.com>
2018-09-27 13:41:27 -0400

    automated commit and push

--- classwork/2018-09-27/stub.py
+++ classwork/2018-09-27/stub.py
@@ -3,12 +3,12 @@
 # K<<NUMBER>> -- <<<FILL THIS OUT>>>
 # <<YEAR-MON-DAY>>
 
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return (render_template("doggy.html"))
 
 if __name__ == "__main__":
     app.debug = True
