3270fa27fc72fc13efeb69844391b1eb0d552e58
Jason Tung <mickeu420@gmail.com>
2018-12-21 02:28:54 -0500

    jsdklfjladf

--- 30_js/dommy.js
+++ 30_js/dommy.js
@@ -4,12 +4,55 @@ var fiblist = document.getElementById("fiblist");
 var b = document.getElementById("b");
 var fb = document.getElementById("fb");
 
+var listevent = (x) =>{
+    x.addEventListener('mouseover', function(){head.innerHTML = this.innerHTML});
+    x.addEventListener('mouseout', function(){head.innerHTML = "Hello World!"});
+    x.addEventListener('click', function(){
+        x.remove();
+        lcount--;
+    });
+};
+
+
+var listele = document.getElementsByTagName("li");
+for (var i = 0; i < listele.length; i++){
+    listevent(listele[i]);
+}
+
+var lcount = 8;
+var fcount = 0;
+
 b.addEventListener('click', function(){
-    var listele = document.getElementsByTagName("li");
-    console.log(listele);
+    //var listele = document.getElementsByTagName("li");
+    console.log(lcount);
     var doggy = document.createElement("li");
-    doggy.innerHTML = "item " + (listele.length);
+    doggy.innerHTML = "item " + (lcount);
     thelist.appendChild(doggy);
-    //add event listener to hover.
+    listevent(doggy);
+    lcount++;
 })
 
+fb.addEventListener('click', function(){
+    //var fbele = document.getElementsByTagName("li");
+    console.log(fcount);
+    var doggy = document.createElement("li");
+    console.log(fcount);
+    doggy.innerHTML = fib(fcount);
+    fiblist.appendChild(doggy);
+    fcount++;
+})
+
+
+var fib = (n) => {
+    var hey = [0, 1];
+    if(n < 2){
+        return hey[n];
+    }else{
+        var inc = 2;
+        while(inc <= n){
+            hey[inc % 2] = hey[(inc + 1) % 2] + hey[inc % 2];
+            inc++;
+        }
+        return hey[(inc - 1) % 2];
+    }
+}
